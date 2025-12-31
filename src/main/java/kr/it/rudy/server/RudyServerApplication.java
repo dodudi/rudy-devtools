7bf80088a081459a76bdf153ef3fd7d855831f27
Rudy <idejrdud@gmail.com>
2025-12-31 11:03:07 +0900

    first commit

---
+++ src/main/java/kr/it/rudy/server/RudyServerApplication.java
@@ -0,0 +1,13 @@
+package kr.it.rudy.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RudyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RudyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/it/rudy/server/RudyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.it.rudy.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RudyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
